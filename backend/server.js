375eb1017bae527032e2fec90a9accc616569217
Danu <danu@prakashs-MacBook-Air.local>
2025-01-07 15:38:25 +0530

    mongoose client

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,23 @@
-if(process.env.NODE_ENV !== 'PRODUCTION'){
+if (process.env.NODE_ENV !== 'PRODUCTION') {
     require('dotenv').config();
 }
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 const connectDataBase = require('./DB/database.js');
-const mongoose=require('mongoose')
 
+app.get('/ping', (req, res) => {
+    return res.send('pong');
+});
 
-app.get('/ping',(request,response)=>{
-    return response.send('pong')
+app.get('/', (req, res) => {
+    const status = mongoose.connection.readyState === 1 ? 'Connected' : 'Not Connected';
+    res.send({ status });
+});
 
-})
-
-app.listen(PORT,()=>{
-    connectDataBase()
-    console.log(`server is running in http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+    connectDataBase();
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
