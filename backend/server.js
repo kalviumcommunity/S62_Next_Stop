97e0e8a36f176209681bb94ff8a3c8974ef6d22c
Danu <danu@prakashs-MacBook-Air.local>
2025-01-07 15:22:54 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
-const express = require('express')
+if(process.env.NODE_ENV !== 'PRODUCTION'){
+    require('dotenv').config();
+}
+
+const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 8080
+const connectDataBase = require('./DB/database.js');
+const mongoose=require('mongoose')
+
+
 app.get('/ping',(request,response)=>{
     return response.send('pong')
 
 })
-const PORT=8080
+
 app.listen(PORT,()=>{
+    connectDataBase()
     console.log(`server is running in http://localhost:${PORT}`)
 })
